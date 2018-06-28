0cdd99d48e078977954abcb3f6e3f0f6a777cc7a
koyo-miyamura <nexus9071@gmail.com>
2018-06-28 17:52:20 +0900

    16日目

--- lib/Jobeet/Controller/Job.pm
+++ lib/Jobeet/Controller/Job.pm
@@ -3,6 +3,7 @@ use Ark 'Controller';
 
 use Jobeet::Models;
 with 'Ark::ActionClass::Form';
+use DateTime::Format::W3CDTF;
 
 sub index :Path {
     my ($self, $c) = @_;
@@ -80,4 +81,16 @@ sub publish :Chained('job') :PathPart {
     $c->redirect( $c->uri_for('/job', $job->token) );
 }
 
+sub atom :Local {
+    my ($self, $c) = @_;
+    $c->res->content_type('application/atom+xml; charset=utf-8');
+
+    $c->stash->{w3c_date} = DateTime::Format::W3CDTF->new;
+    $c->stash->{latest_post} = models('Schema::Job')->latest_post;
+
+    $c->forward('index');
+}
+
 1;
+
+__PACKAGE__->meta->make_immutable;
